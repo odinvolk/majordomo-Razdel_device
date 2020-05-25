bee617e48931e1ac3d3f8dbeaa0badd0a265e51b
Паша <odinwolk@gmail.com>
2020-05-25 16:13:04 +0300

    Create SRazdel_ru.php

---
+++ languages/SRazdel_ru.php
@@ -0,0 +1,19 @@
+<?php
+
+$dictionary = array(
+
+   'SRazdel_PATTERN_BRIGHTNESS' => 'ярк|ярч|яркость',
+   'SRazdel_PATTERN_COLORTEMPERATURE' => 'температур|температура',
+   'LANG_DEVICES_LINK_SENSOR_PASS_BATTERY' => 'Пересылка данных о батареи',
+   'LANG_DEVICES_LINK_SENSOR_PASS_BATTERY_DESCRIPTION' => 'Выставляет свойства в связанном объекте и посылает данные в HomeKit',
+    'DEVICES_LINK_RAZDEL_SWITCH' => 'Управление устройством',
+    'DEVICES_LINK_RAZDEL_SWITCH_DESCRIPTION' => 'Управление другим устройствам в зависимости от статуса раздела',
+    'DEVICES_LINK_RAZDEL_INVERT' => 'Инвертная установка статуса',
+    'LANG_AUTO' => 'Авто',
+);
+
+foreach ($dictionary as $k => $v) {
+ if (!defined('LANG_' . $k)) {
+  @define('LANG_' . $k, $v);
+ }
+}
